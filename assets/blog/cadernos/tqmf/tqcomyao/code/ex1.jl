11739b6d5cc6b453fdd25d67b876a7dc42060b33
lucasvreis <lucasvianareis@gmail.com>
2020-03-29 02:51:07 +0000

    Deploying to master from  - 925fd21c4f84490458ded051b550bb6b549a0014 🚀

--- assets/blog/cadernos/tqmf/tqcomyao/code/ex1.jl
+++ assets/blog/cadernos/tqmf/tqcomyao/code/ex1.jl
@@ -1,9 +1,9 @@
 # This file was generated, do not modify it. # hide
-using LinearAlgebra
+using Yao, SymEngine
 
 # Vamos definir a base computacional como |0⟩ = [1,0] e |1⟩ = [0,1]
-base0 = [1,0]
-base1 = [0,1]
+base0 = ket"0"
+base1 = ket"1"
 estado = normalize(base0 + base1)
 @show estado
 
